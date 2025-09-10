1e1f72746ead60975ecdfb93b4a33f700bd76bc7
raditya-hafid <radityahafid123@gmail.com>
2025-09-10 18:40:06 +0700

    Update tentang.blade.php

--- resources/views/tentang.blade.php
+++ resources/views/tentang.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">\
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>
         body{
             font-family: sans-serif; display: grid; place-items: center; height: 100vh; margin: 0; background-color: #f0f2f5;
