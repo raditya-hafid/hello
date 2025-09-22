4c5cfa734e8b4507619a70427f860caaff099cc7
raditya-hafid <radityahafid123@gmail.com>
2025-09-23 00:37:20 +0700

    create login and logout

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function create(){
+        return view('auth.login');
+    }
+
+    public function store(Request $request){
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->intended('/');
+        }
+
+        //jika gagal
+        return back()->withErrors([
+            'email' => 'Email atau Password salah.'
+        ])->onlyInput('email');
+    }
+
+    public function destroy(Request $request){
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return('/');
+    }
+}
